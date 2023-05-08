7e200a54276ad6ff878fcf58de72d53183dd82cb
Layoute <qingfengroot@163.com>
2023-05-08 13:01:31 +0800

    enhance SegmentControl.kt

--- shared/src/commonMain/kotlin/SegmentControl.kt
+++ shared/src/commonMain/kotlin/SegmentControl.kt
@@ -1,42 +1,38 @@
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
 inline fun <T> SegControl(
     options: MutableList<T>,
     cornerRadius: Dp = 20.dp,
     segColor: Color = Color.Black,
+    padding: Dp = 2.dp,
     modifier: Modifier = Modifier,
+    crossinline onValueChange: (T) -> Unit,
     crossinline content: @Composable (Int, T) -> Unit
 ) {
     val parentSize = remember { mutableStateOf(IntSize.Zero) }
@@ -45,7 +41,7 @@ inline fun <T> SegControl(
         targetValue = offsetX.value,
         animationSpec = tween(durationMillis = 500)
     )
-    var selectedIndex by remember { mutableStateOf(0) }
+    val interactionSource = remember { MutableInteractionSource() }
 
     Box(
         modifier = modifier.layout { measurable, constraints ->
@@ -62,24 +58,35 @@ inline fun <T> SegControl(
         val itemHeight = with(LocalDensity.current) {
             parentSize.value.height.toDp()
         }
+        val parentWidthDp = with(LocalDensity.current) {
+            parentSize.value.width.toDp()
+        }
         Row(modifier = Modifier
             .offset(translationX, 0.dp)
+            .padding(padding)
             .background(segColor, RoundedCornerShape(cornerRadius))
             .width(itemWidth)
-            .height(itemHeight)) {}
+            .height(itemHeight)
+        ) {}
         Row {
             options.forEachIndexed { index, option ->
-                Column (modifier = Modifier.width(itemWidth).height(itemHeight).align(Alignment.CenterVertically), verticalArrangement = Arrangement.Center) {
-                    val color = if (index == selectedIndex) MaterialTheme.colors.primary else MaterialTheme.colors.primaryVariant
-                    ClickableText(
-                        AnnotatedString(option.toString()),
-                        modifier = Modifier.align(Alignment.CenterHorizontally),
-                        style = TextStyle(color = color),
-                        onClick = {
-                            selectedIndex = index
-                            offsetX.value = itemWidth.times(index)
-                        }
-                    )
+                Column (
+                    modifier = Modifier
+                        .padding(padding)
+                        .width(itemWidth)
+                        .height(itemHeight)
+                        .align(Alignment.CenterVertically)
+                        .clickable(onClick = {
+                            if (index == options.size - 1) {
+                                offsetX.value = parentWidthDp.minus(padding * 2).minus(itemWidth)
+                            } else {
+                                offsetX.value = itemWidth.times(index)
+                            }
+                            onValueChange(option)
+                        }, indication = null, interactionSource = interactionSource),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    content(index, option)
                 }
             }
         }
